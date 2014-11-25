877305fde0b49324ec3540c2aae87af60bbc7514
Weeks, Andrew D <andrew.weeks@tdameritrade.com>
2014-11-25 13:53:17 -0600

    Initial Commit (Failing Auth 401) With Api Key

---
+++ InsightlyConsoleDemoApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsightlyConsoleDemoApp {
+    class Program {
+        static void Main(string[] args) {
+            InsightlySDK.Insightly i = new InsightlySDK.Insightly("e94c2e60-310e-483c-bd20-f32ebdb2732a");
+            i.Test();
+        }
+    }
+}
